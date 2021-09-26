6fbe1be3f38c586c75ce7f6bf214f23313970b65
George J. Capnias <george.capnias@outlook.com>
2021-09-26 16:57:26 +0300

    Feature: update program to process command line arguments

--- EmailKit/Program.cs
+++ EmailKit/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace EmailKit
 {
@@ -18,150 +19,194 @@ namespace EmailKit
                 Directory.CreateDirectory(workingFolder);
             }
 
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            foreach (FileInfo fi in di.GetFiles("*.eml"))
+            if (args.Length > 0)
             {
-                MimeMessage message = MimeMessage.Load(fi.FullName);
-                if (message != null)
+                foreach (string item in args)
                 {
-                    string datestamp = message.Date.ToString("yyyyMMdd-HHmmss");
-                    workingFolder = Path.Combine(di.FullName, "working", datestamp);
-                    if (!Directory.Exists(workingFolder))
+                    FileInfo fi = null;
+
+                    if (File.Exists(item))
                     {
-                        Directory.CreateDirectory(workingFolder);
+                        fi = new FileInfo(item);
+                        workingFolder = ProcessMessage(workingFolder, fi);
                     }
-
-                    Console.WriteLine($"Θέμα: {message.Subject}");
-
-                    foreach (MimeEntity bodypart in message.BodyParts)
+                    else
                     {
-                        switch (bodypart)
+                        string filename = Path.Combine(di.FullName, item);
+                        if (File.Exists(filename))
                         {
-                            case TextPart m:
-                                switch (m.ContentType.MimeType.ToLower())
-                                {
-                                    case "text/plain":
-                                        StringBuilder sb = new StringBuilder();
-                                        sb.AppendLine($"Date       : {message.Date.ToString("dddd, dd MMMM yyyy @ HH:hh")}");
-                                        sb.AppendLine($"From       : {message.From.Mailboxes.FirstOrDefault().Name} <{message.From.Mailboxes.FirstOrDefault().Address}>");
-                                        if (message.To.Mailboxes.Count() > 0)
-                                        {
-                                            sb.AppendLine($"To         : {message.To.Mailboxes.FirstOrDefault().Name} <{message.To.Mailboxes.FirstOrDefault().Address}>");
-                                        }
-                                        sb.AppendLine($"Subject    : {message.Subject}");
-                                        if (message.Attachments.Count() > 0)
-                                        {
-                                            sb.Append("Attachments: ");
-                                            foreach (MimeEntity attachment in message.Attachments)
-                                            {
-                                                MimePart file = attachment as MimePart;
-                                                if (file != null)
-                                                {
-                                                    sb.Append($"'{file.FileName}', ");
-                                                }
-                                            }
-                                            sb.AppendLine();
-                                        }
-                                        sb.AppendLine();
-                                        sb.Append(m.Text);
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.txt");
-                                        File.WriteAllText(workingFolder, sb.ToString());
-                                        break;
-
-                                    case "text/html":
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.html");
-                                        File.WriteAllText(workingFolder, m.Text);
-                                        break;
-
-                                    default:
-                                        Console.WriteLine(m.ContentType.MimeType);
-                                        break;
-                                }
-                                break;
-
-                            case MessageDispositionNotification m:
-                                switch (m.ContentType.MimeType)
-                                {
-                                    case "message/disposition-notification":
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "disposition-notification.txt");
-                                        using (Stream file = File.Create(workingFolder))
-                                        {
-                                            m.WriteTo(file);
-                                        }
-                                        break;
-
-                                    default:
-                                        Console.WriteLine(m.ContentType.MimeType);
-                                        break;
-                                }
-                                break;
-
-                            case MimePart m:
-                                switch (m.ContentType.MimeType.ToLower())
-                                {
-                                    case "message/delivery-status":
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "delivery-status.txt");
-                                        using (Stream file = File.Create(workingFolder))
-                                        {
-                                            m.Content.DecodeTo(file);
-                                        }
-                                        break;
-
-                                    default:
-                                        string filename = m.FileName
-                                            .Replace("cid:", "")
-                                            .Replace("?", "")
-                                            .Replace(":", "")
-                                            .Replace("  ", " ")
-                                            .Replace("..", ".")
-                                            .Replace(" .", ".");
-
-                                        filename = string.Concat(filename.Split(Path.GetInvalidFileNameChars()));
-                                        filename = filename.Length > 64 ? filename.Substring(0, 64) : filename;
-
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, filename);
-                                        using (Stream file = File.Create(workingFolder))
-                                        {
-                                            m.Content.DecodeTo(file);
-                                        }
-                                        break;
-                                }
-                                break;
-
-                            case MessagePart m:
-                                switch (m.ContentType.MimeType.ToLower())
-                                {
-                                    case "message/rfc822":
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.eml");
-                                        using (Stream file = File.Create(workingFolder))
-                                        {
-                                            m.WriteTo(file);
-                                        }
-                                        break;
-
-                                    case "text/rfc822-headers":
-                                        workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "headers.txt");
-                                        using (Stream file = File.Create(workingFolder))
-                                        {
-                                            m.WriteTo(file);
-                                        }
-                                        break;
-
-                                    default:
-                                        Console.WriteLine(m.ContentType.MimeType);
-                                        break;
-                                }
-                                break;
-
-                            default:
-                                Console.WriteLine(bodypart.GetType().Name);
-                                break;
+                            fi = new FileInfo(filename);
+                            workingFolder = ProcessMessage(workingFolder, fi);
                         }
                     }
-
+                }
+            }
+            else
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                foreach (FileInfo fi in di.GetFiles("*.eml"))
+                {
+                    workingFolder = ProcessMessage(workingFolder, fi);
                 }
             }
 
+        }
+
+        private static string ProcessMessage(string workingFolder, FileInfo fi)
+        {
+            MimeMessage message = MimeMessage.Load(fi.FullName);
+            if (message != null)
+            {
+                string datestamp = message.Date.ToString("yyyyMMdd-HHmmss");
+                workingFolder = Path.Combine(workingFolder, datestamp);
+                if (!Directory.Exists(workingFolder))
+                {
+                    Directory.CreateDirectory(workingFolder);
+                }
+
+                Console.WriteLine($"Θέμα: {message.Subject}");
+
+                foreach (MimeEntity bodypart in message.BodyParts)
+                {
+                    switch (bodypart)
+                    {
+                        case TextPart m:
+                            switch (m.ContentType.MimeType.ToLower())
+                            {
+                                case "text/plain":
+                                    StringBuilder sb = new StringBuilder();
+                                    sb.AppendLine($"Date       : {message.Date.ToString("dddd, dd MMMM yyyy @ HH:hh")}");
+                                    sb.AppendLine($"From       : {message.From.Mailboxes.FirstOrDefault().Name} <{message.From.Mailboxes.FirstOrDefault().Address}>");
+                                    if (message.To.Mailboxes.Count() > 0)
+                                    {
+                                        sb.AppendLine($"To         : {message.To.Mailboxes.FirstOrDefault().Name} <{message.To.Mailboxes.FirstOrDefault().Address}>");
+                                    }
+                                    sb.AppendLine($"Subject    : {message.Subject}");
+                                    if (message.Attachments.Count() > 0)
+                                    {
+                                        sb.Append("Attachments: ");
+                                        foreach (MimeEntity attachment in message.Attachments)
+                                        {
+                                            MimePart file = attachment as MimePart;
+                                            if (file != null)
+                                            {
+                                                sb.Append($"'{file.FileName}', ");
+                                            }
+                                        }
+                                        sb.AppendLine();
+                                    }
+                                    sb.AppendLine();
+                                    sb.Append(m.Text);
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.txt");
+                                    File.WriteAllText(workingFolder, sb.ToString());
+                                    break;
+
+                                case "text/html":
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.html");
+                                    File.WriteAllText(workingFolder, m.Text);
+                                    break;
+
+                                default:
+                                    Console.WriteLine(m.ContentType.MimeType);
+                                    break;
+                            }
+                            break;
+
+                        case MessageDispositionNotification m:
+                            switch (m.ContentType.MimeType)
+                            {
+                                case "message/disposition-notification":
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "disposition-notification.txt");
+                                    using (Stream file = File.Create(workingFolder))
+                                    {
+                                        m.WriteTo(file);
+                                    }
+                                    break;
+
+                                default:
+                                    Console.WriteLine(m.ContentType.MimeType);
+                                    break;
+                            }
+                            break;
+
+                        case MimePart m:
+                            switch (m.ContentType.MimeType.ToLower())
+                            {
+                                case "message/delivery-status":
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "delivery-status.txt");
+                                    using (Stream file = File.Create(workingFolder))
+                                    {
+                                        m.Content.DecodeTo(file);
+                                    }
+                                    break;
+
+                                default:
+                                    string filename = m.FileName
+                                        .Replace("cid:", "")
+                                        .Replace("?", "")
+                                        .Replace(":", "")
+                                        .Replace("  ", " ")
+                                        .Replace("..", ".")
+                                        .Replace(" .", ".");
+
+                                    filename = string.Concat(filename.Split(Path.GetInvalidFileNameChars()));
+                                    filename = filename.Length > 64 ? filename.Substring(0, 64) : filename;
+
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, filename);
+                                    using (Stream file = File.Create(workingFolder))
+                                    {
+                                        m.Content.DecodeTo(file);
+                                    }
+                                    string hash = MD5Hash(workingFolder);
+                                    Console.WriteLine($"{filename} - MD5: {hash}");
+
+                                    break;
+                            }
+                            break;
+
+                        case MessagePart m:
+                            switch (m.ContentType.MimeType.ToLower())
+                            {
+                                case "message/rfc822":
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "message.eml");
+                                    using (Stream file = File.Create(workingFolder))
+                                    {
+                                        m.WriteTo(file);
+                                    }
+                                    break;
+
+                                case "text/rfc822-headers":
+                                    workingFolder = Path.Combine(fi.Directory.FullName, "working", datestamp, "headers.txt");
+                                    using (Stream file = File.Create(workingFolder))
+                                    {
+                                        m.WriteTo(file);
+                                    }
+                                    break;
+
+                                default:
+                                    Console.WriteLine(m.ContentType.MimeType);
+                                    break;
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine(bodypart.GetType().Name);
+                            break;
+                    }
+                }
+
+            }
+
+            return workingFolder;
+        }
+
+        static string MD5Hash(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(File.ReadAllBytes(filename));
+                return string.Join("", result.Select(x => x.ToString("X2")));
+            }
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,79 @@
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Extract attachments from .eml files using an automated tool.
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+You should have installed .NET Core v3.1 SDK installed. See [Download .NET Core 3.1](https://dotnet.microsoft.com/download/dotnet/3.1) page for details.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/gcapnias/EmailKit.git
+   ```
+2. Restore dependencies
+   ```sh
+   dotnet restore
+   ```
+3. Build project
+   ```sh
+   dotnet build
+   ```
+
+<!-- USAGE EXAMPLES -->
+## Usage
+    Example usage:
+    ```sh
+     ./EmailKit Email01.eml 
+    ```
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- CONTACT -->
+## Contact
+
+George J. - [@gcapnias](https://twitter.com/gcapnias) - email@example.com
+
+Project Link: [https://github.com/gcapnias/EmailKit](https://github.com/gcapnias/EmailKit)
